d502a5ba7faa7a959d3f38c1bebea5e943da841c
Milosz <milosz.wegorski.2021@mumail.ie>
2024-09-24 09:05:21 +0100

    Added file Wave_obj which contains: 	-Sine wave data generation given Phase, amplitude, frequency and offset for a set time interval from time 0 	-Noise generation in a normal distribution with a given noise level Possible future changes: 	-set the number of samples according to time intervals needed as per measurement

---
+++ Wave_obj.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+class Wave:
+    def __init__(self, freq: float, phase: float, amp: float, offset: float, time: float, noise_level : float):
+        self.frequency = freq
+        self.phase = phase
+        self.amp = amp
+        self.offset = offset
+        self.times = np.arange(0, time, time/10000) #time of propagation
+        self.clean_sine_data = (amp * np.sin(self.times * 2 * np.pi* freq + phase)) + offset
+        self.noise_level = noise_level
+        self.noisy_sine_data = self.noisifier(self.times, self.clean_sine_data, noise_level)
+
+    def noisifier(self, times : list[float], amps:list[float], noise_level):
+        noise = np.random.normal(-noise_level, noise_level, len(times))
+        noisy_amps = np.empty_like(amps)
+        for i, j in zip(noise, range(len(amps))):
+            noisy_amps[j] = i + amps[j]
+
+        return noisy_amps
