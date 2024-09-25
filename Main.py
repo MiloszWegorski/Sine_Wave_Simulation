63cafcfccd5df565f0823d52a19c89757f955222
Milosz <milosz.wegorski.2021@mumail.ie>
2024-09-25 10:53:33 +0100

    Added comments to Main.py explaining code

--- Main.py
+++ Main.py
@@ -2,19 +2,22 @@ from Wave_obj import Wave
 import matplotlib.pyplot as plt
 from Measurer import measure
 
+#generating test wave
 wav1 = Wave(5, 0.5, 1, 0.25, 3, 0.02)
 
 print()
 
 fig, ax = plt.subplots(1,2)
 
+#plotting the data
 ax[0].plot(wav1.times, wav1.clean_sine_data)
 ax[1].plot(wav1.times, wav1.noisy_sine_data)
 
-
+#getting test measurements
 results = measure(6, 0.37, wav1.clean_sine_data, wav1.times)
 results_noise = measure(6, 0.37, wav1.noisy_sine_data, wav1.times)
 
+#plotting measurements
 ax[0].scatter(results[0], results[1], color = 'r')
 ax[1].scatter(results_noise[0], results_noise[1], color = 'r')
 
